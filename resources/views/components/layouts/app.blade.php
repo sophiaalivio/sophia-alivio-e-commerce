4e4fb6eeac801cc28af6577b3a3412cfff275227
sophiaalivio <sophiaalivers@gmail.com>
2025-11-05 16:38:14 +0800

    chapter11 e-commerce

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,9 +10,11 @@
     </head>
 
     <body class="bg-slate-200 dark:bg-slate-700">
+    @livewire('partials.navbar')
         <main>
         {{ $slot }}
         </main>
+        @livewire('partials.footer')
         @livewireScripts
     </body>
 </html>
