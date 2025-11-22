439e948a3c5af38dfbd8a627b661ae2d287e5ce9
sophiaalivio <sophiaalivers@gmail.com>
2025-11-22 20:33:14 +0800

    chapter 18-ecommerce

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -29,7 +29,8 @@ class ProductDetailPage extends Component
     //add product to cart method
     public function addToCart($product_id){
 
-        $total_count = CartManagement::addItemToCart($product_id);
+        $total_count = CartManagement::addItemToCartWithQty($product_id, $this->quantity);
+
         $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
 
          $this->alert('success', 'Product added to cart successfully!', [
