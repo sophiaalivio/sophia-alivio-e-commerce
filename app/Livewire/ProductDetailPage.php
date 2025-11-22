d7971135e5bb2ea5e1a8a7960a25cf35aee6ed0a
sophiaalivio <sophiaalivers@gmail.com>
2025-11-22 17:15:40 +0800

    chapter 17-ecommerce

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,16 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\Title;
 use Livewire\Component;
-
+#[Title('Product Detail - AlivioDev')]
 class ProductDetailPage extends Component
 {
+    use LivewireAlert;
     public $slug;
+    public $quantity = 1;
     public function mount($slug){
         $this->slug  = $slug;
-
     }
+
+    public function increaseQty(){
+        $this->quantity++;
+    }
+    public function decreaseQty(){
+        if($this->quantity > 1){
+            $this->quantity--;
+        }
+    }
+    //add product to cart method
+    public function addToCart($product_id){
+
+        $total_count = CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+         $this->alert('success', 'Product added to cart successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+            
+         ]);
+    }
+   
+   
     public function render()
     {
 
