c8e1eb361e6be0a9ccb159d2992b74a5fd56fbbd
sophiaalivio <sophiaalivers@gmail.com>
2025-11-24 17:45:12 +0800

    chapter20 e-commerce

--- app/Livewire/Auth/ForgotPasswordPage.php
+++ app/Livewire/Auth/ForgotPasswordPage.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Password;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Forgot Password')]
 class ForgotPasswordPage extends Component
 {
+    public $email;
+
+    public function save(){
+        $this->validate([
+            'email' => 'required|email|exists:users,email|max:255',
+        ]);
+        $status = Password::sendResetLink(
+            ['email' => $this->email]);
+
+        if($status == Password::RESET_LINK_SENT){
+            session()->flash('success', 'Password reset link sent to your email address!');  
+            $this->email = '';   
+        }
+        }
+
     public function render()
     {
         return view('livewire.auth.forgot-password-page');
--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,8 +17,16 @@
 
           <!-- Form -->
           <form wire:submit.prevent='save'>
+            @if (session('error'))
+              <div class="bg-red-500 dark:bg-red-600 text-sm text-white rounded-lg p-4 mb-4" role="alert">
+                {{ session('error') }}
+               </div>
+            @endif
+
             <div class="grid gap-y-4">
+
               <!-- Form Group -->
+               
               <div>
                 <label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
                 <div class="relative">
@@ -36,6 +44,8 @@
                 @enderror
               </div>
               <!-- End Form Group -->
+
+             
 
               <!-- Form Group -->
               <div>
