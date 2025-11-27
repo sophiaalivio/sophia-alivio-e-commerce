8830de2bbb98dadc0d131022f7750d82238268ce
sophiaalivio <sophiaalivers@gmail.com>
2025-11-27 13:06:33 +0800

    Chapter 22-Ecommerce

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -15,8 +15,7 @@ class OrderStats extends BaseWidget
             Stat::make('New Orders', Order::where('status', 'new')->count()),
             Stat::make('Order Processing', Order::where('status', 'processing')->count()),
             Stat::make('Order Shipped', Order::where('status', 'shipped')->count()),
-            Stat::make('Average Price', Number::currency(Order::avg('grand_total'),'PHP')),
-
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total') ?? 0, 'PHP'))
         ];
     }
 }
